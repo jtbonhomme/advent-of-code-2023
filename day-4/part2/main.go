194c4b8cff6136c1c5b7f04577561a13e3cced06
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2023-12-04 13:23:17 +0100

    day 4 part 2

--- day-4/part2/main.go
+++ day-4/part2/main.go
@@ -4,6 +4,9 @@ import (
 	"bufio"
 	_ "embed"
 	"fmt"
+	"log"
+	"slices"
+	"strconv"
 	"strings"
 )
 
@@ -21,40 +24,96 @@ func isDigit(c byte) bool {
 	return false
 }
 
-func parseLine(line string, n int) int {
-	var number, l int
+func splitNumbers(s string) []int {
+	var numbers = []int{}
 
-	bline := []byte(line)
-	fmt.Printf("%d: %s\n", n, line)
-
-	// parse input
-	for _, c := range bline {
-		if isDigit(c) {
-			number = number*10 + int(c-48)
-			l++
+	stringNumbers := strings.Split(s, " ")
+	for _, sn := range stringNumbers {
+		if sn == "" {
 			continue
+		}
+		n, err := strconv.Atoi(sn)
+		if err != nil {
+			log.Printf("error converting %s: %s", sn, err.Error())
+			continue
+		}
+		numbers = append(numbers, n)
+	}
+
+	return numbers
+}
+
+func parseLine(line string) ([]int, []int) {
+	payload := strings.Split(line, ":")
+	if len(payload) != 2 {
+		panic("line split failed")
+	}
+	payloadSplit := strings.Split(payload[1], "|")
+	if len(payloadSplit) != 2 {
+		panic("payload split failed")
+	}
+
+	winningNumbersStrings := payloadSplit[0]
+	myNumbersStrings := payloadSplit[1]
+
+	winningNumbers := splitNumbers(winningNumbersStrings)
+	myNumbers := splitNumbers(myNumbersStrings)
+
+	return winningNumbers, myNumbers
+}
+
+func score(winningNumbers, myNumbers []int) int {
+	var score int
+	for _, n := range myNumbers {
+		if slices.Contains(winningNumbers, n) {
+			score++
 		}
 	}
 
-	return 0
+	return score
 }
 
-func run(i string) int {
-	var answer, n int
+func displayCards(scratchcards []int) {
+	for i := 0; i < len(scratchcards); i++ {
+		fmt.Printf("Card %d: %d - ", i+1, scratchcards[i])
+	}
+	fmt.Println("")
+}
+
+func run(i string, n int) int {
+	var answer, l int
+	scratchcards := make([]int, n)
+	// init number of scratchcards (eg 1)
+	for i := 0; i < n; i++ {
+		scratchcards[i] = 1
+	}
+
+	fmt.Println(" -- init --")
+	displayCards(scratchcards)
 
 	scanner := bufio.NewScanner(strings.NewReader(i))
 	for scanner.Scan() {
+		fmt.Printf("\n-- parse card %d -- \n", l+1)
 		line := scanner.Text()
-		res := parseLine(line, n)
-		n++
-		answer += res
 		fmt.Println(line)
+		winningNumbers, myNumbers := parseLine(line)
+		numberOfScratchcards := score(winningNumbers, myNumbers)
+		fmt.Printf("score of card %d = %d \n", l+1, numberOfScratchcards)
+		l++
+		for i := l; i < l+numberOfScratchcards; i++ {
+			scratchcards[i] += scratchcards[l-1]
+		}
+		displayCards(scratchcards)
+	}
+
+	for i := 0; i < len(scratchcards); i++ {
+		answer += scratchcards[i]
 	}
 
 	return answer
 }
 
 func main() {
-	answer := run(input)
+	answer := run(input, 196)
 	fmt.Println("Answer: ", answer)
 }
--- day-4/part2/main_test.go
+++ day-4/part2/main_test.go
@@ -4,17 +4,43 @@ import (
 	"testing"
 )
 
+func TestSplitNumbers(t *testing.T) {
+	n1 := splitNumbers("69 72 87 33 61 15  8 78 43 50")
+	if len(n1) != 10 {
+		t.Fatalf("expected n1 to be 10 number long, and got %d", len(n1))
+	}
+	if n1[0] != 69 {
+		t.Errorf("expected n1[0] to be 69, and got %d", n1[0])
+	}
+	if n1[6] != 8 {
+		t.Errorf("expected n1[6] to be 8, and got %d", n1[6])
+	}
+	if n1[9] != 50 {
+		t.Errorf("expected n1[6] to be 8, and got %d", n1[9])
+	}
+}
+
 func TestParse(t *testing.T) {
-	a := parseLine(`line1`, 1)
-	if a != 0 {
-		t.Errorf("expected 0 and got %d", a)
+	line1 := "Card   1: 69 72 87 33 61 15  8 78 43 50 | 96 33 86 53 15 82 50 85 61  8 98 72 43 63 45 78 87 69 10 34 73 88 65 27 19"
+	a, b := parseLine(line1)
+	if len(a) != 10 {
+		t.Errorf("expected winning numbers to be 10 long and got %d", len(a))
+	}
+	if len(b) != 25 {
+		t.Errorf("expected my numbers to be 25 long and got %d", len(b))
 	}
 }
 
 func TestRun(t *testing.T) {
-	a := run(`line1
-line2`)
-	if a != 0 {
-		t.Errorf("expected 0 and got %d", a)
+	var lines = `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`
+
+	a := run(lines, 6)
+	if a != 30 {
+		t.Errorf("expected 30 and got %d", a)
 	}
 }
